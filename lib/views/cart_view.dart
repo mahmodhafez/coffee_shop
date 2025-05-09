241aa42cc353b577a2cd14814e21fc49ed5ca1f2
mahmodhafez <mahmodahmdhafez@gmail.com>
2025-05-09 16:27:38 +0300

    done

--- lib/views/cart_view.dart
+++ lib/views/cart_view.dart
@@ -15,18 +15,22 @@ class CartView extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: const Color(0xFF212325),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          IconButton(
-            onPressed: () {
-              Get.back();
-            },
-            icon: const Icon(Icons.arrow_back_ios_sharp, color: Colors.white38),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            IconButton(
+              onPressed: () {
+                Get.back();
+              },
+              icon: const Icon(
+                Icons.arrow_back_ios_sharp,
+                color: Colors.white38,
+              ),
+            ),
+            SizedBox(height: height * 0.02),
+            Text(
               'Your Cart',
               style: TextStyle(
                 fontSize: 28,
@@ -34,43 +38,46 @@ class CartView extends StatelessWidget {
                 color: Colors.white,
               ),
             ),
-          ),
-          Expanded(
-            child: Obx(() {
-              if (cartController.coffee.isEmpty) {
-                return const Center(
-                  child: Text(
-                    'Your cart is empty!',
-                    style: TextStyle(color: Colors.white38, fontSize: 18),
-                  ),
-                );
-              }
-
-              return ListView.builder(
-                itemCount: cartController.coffee.length,
-
-                padding: EdgeInsets.only(top: 20),
-                itemExtent: height * 0.1,
-                itemBuilder: (context, index) {
-                  final item = cartController.coffee[index];
-                  return ListTile(
-                    leading: Image.asset(item.image, width: width * 0.2),
-                    title: Text(
-                      item.name,
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.remove_circle, color: Colors.red),
-                      onPressed: () {
-                        cartController.removeFromCart(index);
-                      },
+            Expanded(
+              child: Obx(() {
+                if (cartController.coffee.isEmpty) {
+                  return const Center(
+                    child: Text(
+                      'Your cart is empty!',
+                      style: TextStyle(color: Colors.white38, fontSize: 18),
                     ),
                   );
-                },
-              );
-            }),
-          ),
-        ],
+                }
+
+                return ListView.builder(
+                  itemCount: cartController.coffee.length,
+
+                  padding: EdgeInsets.only(top: 50),
+                  itemExtent: height * 0.1,
+                  itemBuilder: (context, index) {
+                    final item = cartController.coffee[index];
+                    return ListTile(
+                      leading: Image.asset(item.image, width: width * 0.2),
+                      title: Text(
+                        item.name,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(
+                          Icons.remove_circle,
+                          color: Colors.red,
+                        ),
+                        onPressed: () {
+                          cartController.removeFromCart(index);
+                        },
+                      ),
+                    );
+                  },
+                );
+              }),
+            ),
+          ],
+        ),
       ),
     );
   }
